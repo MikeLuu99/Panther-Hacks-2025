9f06aa203064e8da10502991bee69cc3e4bc7e87
DUC T LUU <trungducmic@gmail.com>
2025-04-26 17:57:11 -0700

    added instructions in SignInForm.tsx

--- src/app/components/SignInForm.tsx
+++ src/app/components/SignInForm.tsx
@@ -1,20 +1,33 @@
 "use client";
 import { useAuthActions } from "@convex-dev/auth/react";
-import { Card } from "pixel-retroui";
+import { Card, Bubble } from "pixel-retroui";
+import Image from "next/image";
 
 export function SignInForm() {
   const { signIn } = useAuthActions();
 
   return (
     <div className="w-full">
+      <Bubble direction="left" className="w-1/2">
+        Ask me about any health concerns and I will give you a list of
+        challenges to complete! Compete with other patients and earn apples to
+        use in the shop!
+      </Bubble>
+      <Image
+        src="/doctorTalk2.svg"
+        alt="Logo"
+        className="-ml-16"
+        width={100}
+        height={100}
+      />
       <Card className="flex items-center justify-center">
-      <button
-        type="button"
-        className="auth-button"
-        onClick={() => void signIn("anonymous")}
-      >
-        Sign in
-      </button>
+        <button
+          type="button"
+          className="auth-button"
+          onClick={() => void signIn("anonymous")}
+        >
+          Get Started!
+        </button>
       </Card>
     </div>
   );
